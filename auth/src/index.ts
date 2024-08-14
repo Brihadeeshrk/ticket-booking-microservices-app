5ef5ebf0b7e6d7d0aa562a8fe4f0c971a43c42db
briha <hello@briha.xyz>
2024-08-14 12:52:09 +0530

    FEAT & ERROR - the ingress server returns an error / CANNOT GET

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,19 @@ import { json } from "body-parser";
 const app = express();
 app.use(json());
 
+app.get("/api/users/currentuser", async (req, res, next) => {
+  console.log("INSIDE");
+  res.send("hi there");
+});
+
 app.listen(3000, () => {
   console.log("Auth listening on port 3000 🚀");
 });
+
+/**
+ * ROUTES
+ * /api/users/signup
+ * /api/users/signin
+ * /api/users/signout
+ * /api/users/currentuser
+ */
