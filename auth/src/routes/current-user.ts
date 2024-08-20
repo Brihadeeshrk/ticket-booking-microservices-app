501f1b544c6bb2c7a42ff44e9f60cd335be35ff3
briha <hello@briha.xyz>
2024-08-20 20:23:25 +0530

    FEAT - complete the auth service and add 2 middlewares
    middlewares for current user and auth-required

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,20 +1,10 @@
 import express from "express";
-import jwt from "jsonwebtoken";
+import { currentUser } from "../middlewares/current-user";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", async (req, res, next) => {
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
-  }
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    // if the token has been tampered with, payload will throw an error
-    res.send({ currentUser: payload });
-  } catch (error) {
-    res.send({ currentUser: null });
-  }
+router.get("/api/users/currentuser", currentUser, async (req, res, next) => {
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
